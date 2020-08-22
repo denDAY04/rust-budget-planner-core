045761941926c22fa744a2de71073b26a1d8e50d
Andreas Stensig <stensig04@gmail.com>
2020-08-22 19:51:49 +0200

    Docs and stuff

--- src/budget_group.rs
+++ src/budget_group.rs
@@ -1,36 +1,124 @@
-use crate::errors::CollectionError;
+use std::slice::Iter;
+use std::iter::Enumerate;
 use crate::budget_item::BudgetItem;
-use crate::errors::CollectionError::IndexOutOfBounds;
 
-struct BudgetGroup {
+/// An ordered collection of budget items gathered under a common name.
+///
+/// Note that the group does not impose a unique restriction on its entries, so there is no checks
+/// for duplicate entries.
+pub struct BudgetGroup {
     name: String,
     items: Vec<BudgetItem>
 }
 
+/// Error thrown when trying to index the group of budget items with an invalid index.
+pub struct InvalidIndex { }
+
 impl BudgetGroup {
 
-    pub fn new(group_name: String) -> BudgetGroup {
+    /// Create a new budget group with a name.
+    ///
+    /// # Parameters
+    /// * `name` - the desired name of the group.
+    pub fn new(name: &str) -> BudgetGroup {
         BudgetGroup {
-            name: group_name,
+            name: name.to_owned(),
             items: Vec::new()
         }
     }
 
+    /// Get a reference to the group's name.
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    /// Get an enumeration iterator to the items in the group.
+    ///
+    /// This is useful for two reasons:
+    /// 1. To iterate over all the elements in the group, since the underlying container is not
+    /// exposed through the public API.
+    /// 2. To index the elements as they are iterated. This is needed for removing elements using
+    /// [`remove`](#method.remove), as the group is ordered.
+    pub fn enumerate(&self) -> Enumerate<Iter<BudgetItem>> {
+        self.items.iter().enumerate()
+    }
+
+    /// Add a budget item to the group.
+    ///
+    /// Since the group is ordered, adding an item to will trigger a re-ordering of the items in
+    /// the group.
+    ///
+    /// # Parameters
+    /// * `item` - the budget item that should be added to the group.
     pub fn add(&mut self, item: BudgetItem) {
         self.items.push(item);
         self.items.sort_unstable();
     }
 
-    pub fn remove(&mut self, idx: usize) -> Result<(), CollectionError> {
+    /// Remove an item from the group, based on its index.
+    ///
+    /// As the budget items do not contain a notion of identity, the removal has to be done based
+    /// on the item's position in the group.
+    ///
+    /// # Parameters
+    /// * `idx` - the index of the item to be removed, as discovered using [`enumerate`](#method.enumerate).
+    ///
+    /// # Returns
+    /// `Result::Ok()` if the index is valid and an item was therefore removed, or `Result::Err` if
+    /// the index is invalid.
+    pub fn remove(&mut self, idx: usize) -> Result<(), ()> {
         if idx > self.items.len() {
-            return Err(IndexOutOfBounds)
+            return Err(());
         }
         self.items.remove(idx);
         self.items.sort_unstable();
         Ok(())
     }
+}
 
-    pub fn items(&self) -> &Vec<BudgetItem> {
-        &self.items
+#[cfg(test)]
+mod test {
+    use crate::budget_group::BudgetGroup;
+    use crate::budget_item::{BudgetItem, Period};
+
+    #[test]
+    fn new() {
+        let group = BudgetGroup::new("foo");
+        assert_eq!("foo", group.name());
+    }
+
+    #[test]
+    fn add() {
+        let mut item_group = BudgetGroup::new("foo");
+        assert_eq!(0, item_group.enumerate().len());
+
+        let item = BudgetItem::with_income("bar", 10.0, Period::Every1Month);
+        item_group.add(item);
+
+        assert_eq!(1, item_group.enumerate().len())
+    }
+
+    #[test]
+    fn list_is_ordered() {
+        let mut item_group = BudgetGroup::new("foo");
+
+        let item1 = BudgetItem::with_income("qq", 10.0, Period::Every1Month);
+        let item2 = BudgetItem::with_income("ab", 10.0, Period::Every1Month);
+        let expected_first = item2.clone();
+        item_group.add(item1);
+        item_group.add(item2);
+
+        assert_eq!(&expected_first, item_group.enumerate().next().unwrap().1, "Unexpected first budget item")
+    }
+
+    #[test]
+    fn remove() {
+        let mut item_group = BudgetGroup::new("foo");
+        let item = BudgetItem::with_income("bar", 10.0, Period::Every1Month);
+        item_group.add(item);
+
+        let removed = item_group.remove(0);
+        assert!(removed.is_ok());
+        assert_eq!(0, item_group.enumerate().len())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
-mod errors;
-mod budget_item;
-mod budget_group;
+//! The core modules for the rust budget planner.
+//!
+//! This crate contains all the business logic for the simple rust budget planner and is meant to
+//! be integrated with external UI executables or libraries.
 
+/// Module holding the core budget item type that acts as entries in a budget group.
+pub mod budget_item;
 
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+/// Module holding the core budget group types that manages a collection of budget items.
+pub mod budget_group;
